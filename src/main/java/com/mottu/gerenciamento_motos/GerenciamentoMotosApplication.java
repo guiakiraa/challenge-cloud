cd99a271e667c0b7f67eb572d2099b0253a62890
Laboratório FIAP <labsfiap@fiap.com.br>
2025-05-07 08:49:15 -0300

    feat: configuracoes iniciais

--- src/main/java/com/mottu/gerenciamento_motos/GerenciamentoMotosApplication.java
+++ src/main/java/com/mottu/gerenciamento_motos/GerenciamentoMotosApplication.java
@@ -2,7 +2,13 @@ package com.mottu.gerenciamento_motos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableCaching
+@EnableJpaRepositories
+@EntityScan
 @SpringBootApplication
 public class GerenciamentoMotosApplication {
 
