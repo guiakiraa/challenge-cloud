5e6f1b7db8b7f32d94b1960a9073923bafe4626f
Laboratório FIAP <labsfiap@fiap.com.br>
2025-05-07 08:15:53 -0300

    first commit

---
+++ src/main/java/com/mottu/gerenciamento_motos/GerenciamentoMotosApplication.java
@@ -0,0 +1,13 @@
+package com.mottu.gerenciamento_motos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentoMotosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciamentoMotosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mottu/gerenciamento_motos/GerenciamentoMotosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mottu.gerenciamento_motos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoMotosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
